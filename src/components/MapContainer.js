e12fa9e098fc69feb945239c2a837462a1b7bae5
Yao-Hsien Hsieh <laxative0612@gmail.com>
2019-05-29 14:08:30 +0800

    mainMap initialization and pixi.js lib import

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,1 +1,28 @@
+import * as PIXI from 'pixi.js'
 // map
+
+// width and height setting
+const [ initWidth, initHeight ] = [ window.innerWidth, window.innerHeight ]
+
+// Pixi application setting, set it as auto resize render
+const app = new PIXI.Application({
+  width: initWidth,
+  height: initHeight,
+  backgroundColor: 0xffffff,
+  autoResize: true,
+  resolution: devicePixelRatio
+})
+
+// add container
+let container = new PIXI.Container()
+app.stage.addChild(container)
+app.container = container
+
+// resize function setting
+window.addEventListener('resize', () => {
+  const { innerWidth, innerHeight } = window      // get current window size
+  app.renderer.resize(innerWidth, innerHeight)    // resize canvas
+  app.container.scale.set(Math.max(innerWidth / initWidth, innerHeight / initHeight)) // resize all items inside canvas
+})
+
+export default app
--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 // import style.scss file
 import './style.scss'
+import mainMap from './components/MapContainer'
 
-const { innerWidth, innerHeight } = window
-const app = new PIXI.Application({ width: innerWidth, height: innerHeight })
 const root = document.body.querySelector('#app')
-root.appendChild(app.view)
+root.appendChild(mainMap.view)
 
-window.addEventListener('resize', () => {
-  const { innerWidth, innerHeight } = window
-  app.renderer.resize(innerWidth, innerHeight)
-})
